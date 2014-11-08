f553f8f152e26f33db46b1ca821b5e4c74d2552a
Blaine Lewis <blaine.lewis.1@outlook.com>
2014-11-07 23:38:04 -0800

    Agents can now make actions

--- grid.js
+++ grid.js
@@ -1,7 +1,6 @@
-
 var colors = {'W' : 'blue', 'G' : 'chocolate'};
 
-var agent = {'x' : 1, 'y' : 1}
+var agents = [{'x' : 1, 'y' : 1, 'act':null}]
 
 var map = [ ['W','W','W','W','W','W','W','W','W','W'],
 			['W','G','G','G','G','G','G','G','G','W'],
@@ -23,6 +22,12 @@ var SIZE = 50;
 $(function() {
 
 	timer = setTimeout(displayAndAct, time);
+
+	agents[0].act = BlaineBotAct;
+
+	console.log(agents);
+	console.log(agents[0]);
+	console.log(agents[0].act);
 
 	display();
 
@@ -62,7 +67,9 @@ function displayAndAct() {
 }
 
 function agentsAct() {
-	
+	for(var i = 0; i < agents.length; i++){
+		agents[i].act(agents[i]);
+	}
 }
 
 function display() {
@@ -79,8 +86,69 @@ function display() {
 		}
 	}
 
-	//Draw our agent too :D
 	ctx.fillStyle = 'yellow';
-	ctx.fillRect(agent.x*SIZE + 10, agent.y*SIZE + 10, SIZE - 20, SIZE - 20);
+
+	for(var i = 0; i < agents.length; i++){
+		ctx.fillRect(agents[i].x*SIZE + 10, agents[i].y*SIZE + 10, SIZE - 20, SIZE - 20);
+	}
+}
+
+function getState(agent) {
+	return agent.x + agent.y*2;
+}
+
+function getActions(agent) {
+	//search neighbours... etc. 
+	//Are we on a food block..
+	var actions = [];
+
+	var cur = map[agent.y][agent.x];
+
+	if(agent.x + 1 < map[agent.y].length){
+		if(map[agent.y][agent.x + 1] == cur) {
+			actions.push(right);
+		}
+	}
+
+	if(agent.y + 1 < map.length) {
+		if(map[agent.y + 1][agent.x] == cur) {
+			actions.push(down);
+		}
+	}
+
+	if(agent.x - 1 > 0){
+		if(map[agent.y][agent.x + 1] == cur) {
+			actions.push(left);
+		}
+	}
+
+	if(agent.y - 1 > 0){
+		if(map[agent.y - 1][agent.x] == cur) {
+			actions.push(up);
+		}
+	}
+
+
+
+	return actions;
+}
+
+/* list of actions? */
+
+function left(agent) {
+	agent.x -= 1;
+}
+
+function right(agent) {
+	agent.x += 1;
+
+}
+
+function up(agent) {
+	agent.y -= 1;
+}
+
+function down(agent) {
+	agent.y += 1;
 }
 
