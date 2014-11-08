658b44e3b101618cda7fd14c8b138b167db58b72
Blaine Lewis <blaine.lewis.1@outlook.com>
2014-11-07 22:27:58 -0800

    Got visual things working

--- grid.js
+++ grid.js
@@ -1,7 +1,7 @@
 
 var colors = {'W' : 'blue', 'G' : 'chocolate'};
 
-console.log(colors);
+var agent = {'x' : 1, 'y' : 1}
 
 var map = [ ['W','W','W','W','W','W','W','W','W','W'],
 			['W','G','G','G','G','G','G','G','G','W'],
@@ -12,46 +12,75 @@ var map = [ ['W','W','W','W','W','W','W','W','W','W'],
 			['W','G','G','G','G','G','G','G','G','W'],
 			['W','G','G','G','G','G','G','G','G','W'],
 			['W','G','G','G','G','G','G','G','G','W'],
-			['W','W','W','W','W','W','W','W','W','G']
+			['W','W','W','W','W','W','W','W','W','W']
 			];
-
-console.log(map);
 
 var timer;
 var time = 1000;
-var SIZE = 20;
+var SIZE = 50;
 
 
 $(function() {
-	//set timer for event loop
+
 	timer = setTimeout(displayAndAct, time);
 
-	//onclick for time modifiers start stop...
+	display();
+
+
+	$('#faster').click(faster);
+	$('#slower').click(slower);
+	$('#play').click(play);
+	$('#pause').click(pause);
 
 });
+
+function pause() {
+	clearTimeout(timer);
+}
+
+function play() {
+	clearTimeout(timer);
+	setTimeout(displayAndAct, time);
+}
+
+function faster() {
+	time -= 100;
+}
+
+function slower() {
+	time += 100;
+}
 
 function displayAndAct() {
 	clearTimeout(timer);
 
+	agentsAct();
+
 	display();
 
-	//setTimeout(displayAndAct, time);
+	setTimeout(displayAndAct, time);
+}
+
+function agentsAct() {
+	
 }
 
 function display() {
 	var $canvas = document.getElementById("grid");
 	var ctx = $canvas.getContext("2d");
-
+	ctx.strokeStyle = "black";
 
 	for(var y = 0; y < map.length; y++) {
 		for(var x = 0; x < map[y].length; x++){
-			console.log(colors[map[y][x]] + "X: " + x + ", Y: "+ y);
 			ctx.fillStyle = colors[map[y][x]];
+			ctx.fillRect(x*SIZE, y*SIZE, SIZE, SIZE);
+			ctx.strokeRect(x*SIZE, y*SIZE, SIZE, SIZE);
 
-			//ctx.strokeStyle = "black";
-			ctx.rect(x*SIZE, y*SIZE, SIZE, SIZE);
-			ctx.fill();
 		}
 	}
 
+	//Draw our agent too :D
+	ctx.fillStyle = 'yellow';
+	ctx.fillRect(agent.x*SIZE + 10, agent.y*SIZE + 10, SIZE - 20, SIZE - 20);
 }
+
