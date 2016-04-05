97bfbd13c4f1b62a4eb1862c8e7804d42351d2da
Blaine Lewis <blaine.lewis.1@outlook.com>
2016-04-05 23:31:53 +1000

    added better about me, changed assets

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,5 +1,11 @@
 var blaineLewis = angular.module('BlaineLewis', []);
 
+blaineLewis.filter('to_trusted', ['$sce', function($sce){
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    }]);
+
 blaineLewis.controller('Controller', ['$scope', function ($scope) {
   $scope.experiences = experiences;
   $scope.plays = plays;
